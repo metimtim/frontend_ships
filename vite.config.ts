aed73441a5a6f498b62bc437addae5baca1e8884
metimtim <pronchenko04@gmail.com>
2024-12-09 19:34:01 +0300

    the end

--- vite.config.ts
+++ vite.config.ts
@@ -3,22 +3,29 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  define: {
+    'process.env': process.env,  // Прокси переменные окружения
+  },
   plugins: [react()],
-  base: "/frontend_ships",
+  base:'/',
   server: {
     host: "0.0.0.0",
     port: 3000,
     proxy: {
       "/api": {
-        target: "http://localhost:8000/web",
+        target: "http://127.0.0.1:8000",
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, "/"),
       },
       '/tauri-api': {
-        target: 'http://localhost:8000/tauri',
+        target: 'http://127.0.0.1:3002',
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/tauri-api/, ''),
       },
     },
+  },
+  build: {
+    outDir: 'dist',  // Папка для сборки
+    emptyOutDir: true, // Очищать папку перед сборкой
   },
 });
